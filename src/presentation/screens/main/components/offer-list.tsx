fba58e87be6f2877e9add262efa1f4033c5d1ca2
Vladimir Boyko <ya.vladimir565@mail.ru>
2024-09-23 03:26:40 +0500

    Добавлены страницы приложения

---
+++ src/presentation/screens/main/components/offer-list.tsx
@@ -0,0 +1,49 @@
+import { Offer } from '../../../../domain/models/offer';
+import { OfferItem } from './offer-item';
+
+export type OfferListProps = {
+  offers: Offer[];
+}
+
+export function OfferList({ offers }: OfferListProps) {
+  return (
+    <div className="cities">
+      <div className="cities__places-container container">
+        <section className="cities__places places">
+          <h2 className="visually-hidden">Places</h2>
+          <b className="places__found">{offers.length} places to stay in Amsterdam</b>
+          <form className="places__sorting" action="#" method="get">
+            <span className="places__sorting-caption">Sort by</span>
+            <span className="places__sorting-type" tabIndex={0}>
+              Popular
+              <svg className="places__sorting-arrow" width="7" height="4">
+                <use xlinkHref="#icon-arrow-select"></use>
+              </svg>
+            </span>
+            <ul className="places__options places__options--custom places__options--opened">
+              <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+              <li className="places__option" tabIndex={0}>Price: low to high</li>
+              <li className="places__option" tabIndex={0}>Price: high to low</li>
+              <li className="places__option" tabIndex={0}>Top rated first</li>
+            </ul>
+          </form>
+          <div className="cities__places-list places__list tabs__content">
+            {
+              offers.map((offer: Offer) =>
+                (
+                  <OfferItem
+                    key={offer.title}
+                    offer={offer}
+                  />
+                )
+              )
+            }
+          </div>
+        </section>
+        <div className="cities__right-section">
+          <section className="cities__map map"></section>
+        </div>
+      </div>
+    </div>
+  );
+}
