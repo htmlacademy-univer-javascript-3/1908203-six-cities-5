30fb1077b28210e9548c916a4ebaeb1f9672cfb9
Vladimir Boyko <ya.vladimir565@mail.ru>
2024-10-17 14:56:31 +0500

    Исправит остальные замечания с ревью

--- src/presentation/app.tsx
+++ src/presentation/app.tsx
@@ -2,20 +2,21 @@ import { FavoritesPage } from './screens/favorites/favorites-page';
 import { LoginPage } from './screens/login/login-page';
 import { MainPage } from './screens/main/main-page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { NotFoundPage } from './screens/not_found/not-found';
+import { NotFoundPage } from './screens/not-found/not-found';
 import { OfferPage } from './screens/offer/offer-page';
 import { PrivateRoute } from '../routing/private-route';
+import { AppRoutes } from '../routing/app-routes';
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/' element={<MainPage />} />
-        <Route path='/offer/:id' element={<OfferPage />} />
-        <Route path='/favorites'
+        <Route path={AppRoutes.login} element={<LoginPage />} />
+        <Route path={AppRoutes.main} element={<MainPage />} />
+        <Route path={AppRoutes.order} element={<OfferPage />} />
+        <Route path={AppRoutes.favorites}
           element={
-            <PrivateRoute>
+            <PrivateRoute isAuthenticated={false}>
               <FavoritesPage />
             </PrivateRoute>
           }
--- src/routing/private-route.tsx
+++ src/routing/private-route.tsx
@@ -1,11 +1,11 @@
 import { Navigate } from 'react-router-dom';
+import { AppRoutes } from './app-routes';
 
 type PrivateRouteProps = {
+  isAuthenticated: boolean;
   children: JSX.Element;
 }
 
-export function PrivateRoute({children}: PrivateRouteProps): JSX.Element {
-  const authorized = false;
-
-  return authorized ? children : <Navigate to={'/login'} />;
+export function PrivateRoute({isAuthenticated, children}: PrivateRouteProps): JSX.Element {
+  return isAuthenticated ? children : <Navigate to={AppRoutes.login} />;
 }
